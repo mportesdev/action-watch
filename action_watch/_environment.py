1ee33eb8b5433ab059e900df0a68f5abc467a89e
Michal Porteš <michalportes1@gmail.com>
2023-06-10 23:15:44 +0200

    Remove .env-default
    Store the contents of the default .env file in _environment.py.

--- action_watch/_environment.py
+++ action_watch/_environment.py
@@ -1,13 +1,38 @@
 import os
-import shutil
-from importlib.resources import files
 
 from dotenv import load_dotenv
 
 from ._paths import CONFIG_DIR
 
-DEFAULT_DOTENV = files('action_watch') / '.env-default'
 DOTENV = CONFIG_DIR / '.env'
+
+DEFAULT_DOTENV = '''
+# Path to search recursively for `.github/workflows/*.yml` files.
+# If empty or not set, falls back to current working directory.
+# Example: ~/projects/python/
+ACTION_WATCH_DISCOVERY_ROOT=
+
+# Git credential helper command to be used by an authentication handler to authenticate to GitHub.
+# See https://pypi.org/project/helper-auth/ for more info.
+# Example: git credential-github
+ACTION_WATCH_AUTH_HELPER=
+
+# String to be used as the Authorization header of HTTP requests to authenticate to GitHub.
+# Ignored if ACTION_WATCH_AUTH_HELPER is set.
+# Example: Bearer YourGitHubToken
+ACTION_WATCH_AUTH_HEADER=
+
+# The following are boolean flags. Use 0 or 1.
+
+# Cache the paths of the discovered workflow files.
+ACTION_WATCH_CACHE_PATHS=1
+
+# Cache the GitHub API requests and responses.
+ACTION_WATCH_CACHE_REQUESTS=1
+
+# Output debug messages to stderr.
+ACTION_WATCH_DEBUG=0
+'''.lstrip()
 
 
 def _setup_env():
@@ -16,7 +41,7 @@ def _setup_env():
     """
     if not DOTENV.is_file():
         CONFIG_DIR.mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(DEFAULT_DOTENV, DOTENV)
+        DOTENV.write_text(DEFAULT_DOTENV, encoding='utf8')
     load_dotenv(DOTENV)
 
 
